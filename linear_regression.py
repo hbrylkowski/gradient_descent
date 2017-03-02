fdff3093a38358193e0838b43bbf69101cfeaa1c
Hubert Bryłkowski <hubert.brylkowski@brainly.com>
2017-03-02 12:30:06 +0100

    normalization of x

--- linear_regression.py
+++ linear_regression.py
@@ -9,15 +9,15 @@ def err_func(houses, x, a, b):
 
 
 def normalize(set):
-    max_value = max(set)
-    return list(map(lambda x: x/max_value, set))
+    max_value = max(set, key=lambda p: p[1])
+    return list(map(lambda p: (p[0] / len(set), p[1]/max_value[1]), set))
 
 values = []
-BATCH_SIZE = 300
-LEARNING_RATE = 0.0000003 / BATCH_SIZE
-ITERATIONS = 3000000
+BATCH_SIZE = 60
+LEARNING_RATE = 1e-3
+ITERATIONS = 1500000
 for x in range(BATCH_SIZE):
-    values.append(math.sin(math.pi * x / BATCH_SIZE) + random.random() / 10)
+    values.append((x, math.sin(math.pi * x / BATCH_SIZE) + random.random() / 10))
 
 values = normalize(values)
 
@@ -32,10 +32,10 @@ for i in range(ITERATIONS):
     for x in range(len(values) - 1):
         y = 0
         for v in range(polynomial_level):
-            y += coefficients[v] * (x ** v)
-        diff = y - values[x]
+            y += coefficients[v] * (values[x][0] ** v)
+        diff = y - values[x][1]
         for power in range(polynomial_level):
-            gradient[power] += diff * (x ** power)
+            gradient[power] += diff * (values[x][0] ** power)
         error += abs(diff)
 
     for c in range(polynomial_level):
--- plot_results.py
+++ plot_results.py
@@ -12,12 +12,12 @@ calculated_line = []
 for x in range(len(points)):
     y = 0
     for v in range(len(coefficients)):
-        y += coefficients[v] * (x ** v)
+        y += coefficients[v] * (points[x][0] ** v)
     calculated_line.append(y)
 
 plt.subplot(1, 2, 1)
 plt.plot(errors)
 plt.subplot(1, 2, 2)
-plt.plot(calculated_line)
-plt.plot(range(len(points)), points, 'ro', markersize=2)
+plt.plot(list(map(lambda p: p[0], points)), calculated_line)
+plt.plot(list(map(lambda p: p[0], points)), list(map(lambda p: p[1], points)), 'ro', markersize=2)
 plt.show()
